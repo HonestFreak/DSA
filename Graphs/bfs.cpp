ae0b0cdc02b0583a0ff5eb31cb9c68fab52173ad
honestfreak <me21b006@iittp.ac.in>
2023-07-17 10:36:05 +0530

    breath first search

--- Graphs/bfs.cpp
+++ Graphs/bfs.cpp
@@ -3,50 +3,53 @@
 
 using namespace std;
 
-class Graph{
+class Graph {
   int numVertices;
-  vector<int>* adjLists;
-  bool* visited;
+  vector<int> *adjLists;
 
-  public:
-  Graph(int vertices){
+public:
+  Graph(int vertices) {
     numVertices = vertices;
     adjLists = new vector<int>[vertices];
   }
 
-  void addEdge(int src, int dest){
+  void addEdge(int src, int dest) {
     adjLists[src].push_back(dest);
     adjLists[dest].push_back(src);
   }
 
-  void BFS(int StartVertex){
+  void BFS(int StartVertex) {
     vector<bool> visited(numVertices, false);
     vector<int> queue;
-    
-    while(!queue.empty()){
-      int curvertex = queue.front();
-      cout<<"visited" << curvertex<<endl;
-      queue.pop_front();
 
-      for(auto i=adjLists[curvertex].begin(); i!=adjLists[curvertex].end(); i++){
+    visited[StartVertex] = true;
+    queue.push_back(StartVertex);
+
+    while (!queue.empty()) {
+      int curvertex = queue.front();
+      cout << "visited " << curvertex << endl;
+      queue.erase(queue.begin());
+
+      for (auto i = adjLists[curvertex].begin(); i != adjLists[curvertex].end(); i++) {
         int adjVertex = *i;
-        if(!visited[adjVertex]){
-          visited[adjVertex]=true;
+        if (!visited[adjVertex]) {
+          visited[adjVertex] = true;
           queue.push_back(adjVertex);
         }
       }
     }
   }
-}
+};
 
-int main()
-{
+int main() {
   Graph g(4);
-  g.addEdge(0,1);
-  g.addEdge(0,2);
-  g.addEdge(1,2);
-  g.addEdge(2,0);
-  g.addEdge(2,3);
-  g.addEdge(3,3);
- return 0;
+  g.addEdge(0, 1);
+  g.addEdge(0, 2);
+  g.addEdge(1, 2);
+  g.addEdge(2, 0);
+  g.addEdge(2, 3);
+  g.addEdge(3, 3);
+  g.BFS(2);  // Starting vertex for BFS
+
+  return 0;
 }
